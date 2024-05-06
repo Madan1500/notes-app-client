9a9e0bf5effac5e14c474a7474c1fcdc0f839610
Madan Mohan Behera <madanmohan14072002@gmail.com>
2024-05-07 00:27:24 +0530

    changed the alert message which was showing before

--- src/containers/Signup.js
+++ src/containers/Signup.js
@@ -42,7 +42,6 @@ export default function Signup() {
             setIsLoading(false);
             setNewUser(newUser);
         } catch (e) {
-            onError(e);
             if(e.name === "UsernameExistsException"){
                 try {
                     await Auth.resendSignUp(fields.email);
